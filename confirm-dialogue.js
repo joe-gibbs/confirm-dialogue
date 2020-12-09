4c6a59a023bcee5298e15747a3067759ced3b79b
Joe Gibbs <joe.gibbs0@gmail.com>
2020-12-09 11:00:26 +1100

    Added more feautures to confirm dialogue

--- confirm-dialogue.js
+++ confirm-dialogue.js
@@ -1,5 +1,38 @@
 export class ConfirmDialogue {
     constructor(confirm, callback) {
+        const head = document.createElement("div");
+        head.classList.add("reusable-component");
+
+        const background = document.createElement("div");
+        background.classList.add("background");
+        head.appendChild(background);
+
+        const dialogue = document.createElement("div");
+        dialogue.classList.add("dialogue");
+
+        const text = document.createElement("p");
+        text.innerText = confirm;
+        dialogue.appendChild(text);
         
+        const confirmButton = document.createElement("button");
+        confirmButton.innerText = "Yes";
+        confirmButton.onclick = function() {
+            callback(true);
+            head.remove();
+        }
+
+        const denyButton = document.createElement("button");
+        denyButton.innerText = "No";
+        denyButton.onclick = function() {
+            callback(false);
+            head.remove();
+        }
+
+        dialogue.appendChild(confirmButton);
+        dialogue.appendChild(denyButton);
+
+        head.appendChild(dialogue);
+
+        document.body.prepend(head);
     }
 }
